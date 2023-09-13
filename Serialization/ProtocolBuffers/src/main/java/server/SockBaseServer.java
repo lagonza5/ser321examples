4011720074687ee3fac4c4498924030321f7170a
lagonza5 <lagonza5@asu.edu>
2023-09-12 18:07:22 -0700

    Add useful comments t src/main/java/server/SockBaseServer.java file

--- Serialization/ProtocolBuffers/src/main/java/server/SockBaseServer.java
+++ Serialization/ProtocolBuffers/src/main/java/server/SockBaseServer.java
@@ -12,12 +12,19 @@ class SockBaseServer {
     public static void main (String args[]) throws Exception {
 
         int count = 0;
-        ServerSocket serv = null;
+
+        ServerSocket serv = null; //server socket
+
         InputStream in = null;
         OutputStream out = null;
-        Socket clientSocket = null;
+
+        Socket clientSocket = null; //client socket
+
         int port = 9099; // default port
+
         int sleepDelay = 10000; // default delay
+
+        //check args
         if (args.length != 2) {
             System.out.println("Expected arguments: <port(int)> <delay(int)>");
             System.exit(1);
@@ -42,52 +49,63 @@ class SockBaseServer {
 
         //I haven't seen these conditions yet...
         while (serv.isBound() && !serv.isClosed()) {
-            System.out.println("Ready...");
+            System.out.println("Ready..."); //for debugging
+
             try {
+                //set up client socket connection and streams for input/output
                 clientSocket = serv.accept();
                 in = clientSocket.getInputStream();
                 out = clientSocket.getOutputStream();
 
-                // read the proto object and put into new object (from file operation.proto and its definitions)
-                Operation op = Operation.parseDelimitedFrom(in);
+                // read the stream message and create a proto object from it (from file operation.proto and its definitions)
+                Operation op = Operation.parseDelimitedFrom(in); //proto library function
 
                 String result = null;
-                String num1 = op.getVal1();
-                String num2 = op.getVal2();
-                int baseN = op.getBase();
+                String num1 = op.getVal1(); //proto library function, created by the proto library due to our .proto file definitions
+                String num2 = op.getVal2(); //proto library function
+                int baseN = op.getBase(); //proto library function
 
-                //Base.java contains code for all the valid operations and for converting numbers to appropriate formats
+                //Base.java contains code for all the valid operations and for converting numbers to appropriate numerical formats
                 Base base = new Base();
 
                 //The enum types come directly from the 'Operation' protobuf file
                 if (op.getOperationType() == Operation.OperationType.ADD) {
-                  result = base.add(num1, num2, baseN);
-                  System.out.println("base " + baseN + ": " + num1 + " + " + num2 + " = " + result);
+                    //proto library function, created by the proto library due to our .proto file definitions for enum type
+                    result = base.add(num1, num2, baseN);
+                    System.out.println("base " + baseN + ": " + num1 + " + " + num2 + " = " + result);
                 } else if (op.getOperationType() == Operation.OperationType.SUB) {
-                  result = base.substract(num1, num2, baseN);
-                  System.out.println("base " + baseN + ": " + num1 + " - " + num2 + " = " + result);
+                    result = base.substract(num1, num2, baseN);
+                    System.out.println("base " + baseN + ": " + num1 + " - " + num2 + " = " + result);
                 }
                 if (op.getResponseType() == Operation.ResponseType.JSON){
-                  //just building a JSON string
-                  result = "{'result':'" + result +"'}";
+                    //proto library function, created by the proto library due to our .proto file definitions
+
+                    //just building a JSON string
+                    result = "{'result':'" + result +"'}";
                 }
-                Response response = buildResponse(result);
+
+
+                Response response = buildResponse(result); //custom function that uses proto library functions within
+
+                //proto library function to write data to an output stream
                 response.writeDelimitedTo(out);
+
             } catch (Exception ex) {
-                ex.printStackTrace();
+                  ex.printStackTrace();
             } finally {
-                if (out != null)  out.close();
-                if (in != null)   in.close();
-                if (clientSocket != null) clientSocket.close();
+                  if (out != null)  out.close();
+                  if (in != null)   in.close();
+                  if (clientSocket != null) clientSocket.close();
+                  //close resources
             }
         }
     }
 
     //server only has to build responses to client requests/messages
     private static Response buildResponse(String result) {
-      Response.Builder response = Response.newBuilder();
-      response.setResultString(result);
-      return response.build();
+      Response.Builder response = Response.newBuilder(); //proto library functions
+      response.setResultString(result); //proto library function
+      return response.build(); //proto library function
     }
 }
 
